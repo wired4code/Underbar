9aa1429d3592d4b05ec781af4904e1cb3a0355fd
Sonny Sheth <corolla90@hotmail.com>
2015-11-19 09:56:57 -0200

    Add missing code for _.memoize function to pass test

--- src/underbar.js
+++ src/underbar.js
@@ -315,6 +315,21 @@
   // already computed the result for the given argument and return that value
   // instead if possible.
   _.memoize = function(func) {
+
+    var result = {};
+
+    return function(val){
+
+      if(val in result){
+        return result[val];
+      }
+
+      else {
+
+        result[val] = func.apply(this, arguments);
+        return result[val];
+      }
+    }
   };
 
   // Delays a function for the given number of milliseconds, and then calls
