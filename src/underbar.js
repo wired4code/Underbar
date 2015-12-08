1aac8d3eeabedcfcb3ae1fed71356638bec6386c
Sonny Sheth <sonny.sheth@gmail.com>
2015-12-08 12:33:31 -0200

    Complete _.flatten function to pass tests

--- src/underbar.js
+++ src/underbar.js
@@ -451,6 +451,16 @@
   //
   // Hint: Use Array.isArray to check if something is an array
   _.flatten = function(nestedArray, result) {
+    var result = result || [];
+    for(var i = 0; i < nestedArray.length; i++){
+      if(Array.isArray(nestedArray[i])){
+        result = _.flatten(nestedArray[i], result);
+      }
+      else {
+        result.push(nestedArray[i]);
+      }
+    }
+    return result;
   };
 
   // Takes an arbitrary number of arrays and produces an array that contains
